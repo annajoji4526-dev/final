b3f13c1acd8e240b175103b2ab220473efca52bf
annajoji4526-dev <annajoji4526@gmail.com>
2026-01-14 15:22:54 +0530

    connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://<annajoji4526_db_user>:<9JxhpXPoGqKSgc7u>@final.u4qkidx.mongodb.net/?retryWrites=true&w=majority")
+  .connect("mongodb+srv://annajoji4526_db_user:9JxhpXPoGqKSgc7u@final.u4qkidx.mongodb.net/?appName=final")
   .then(() => {
     console.log("Connected to DB");
   })
   .catch((error) => {
-    console.error("DB connection error:", error);
+    console.log(error);
   });
